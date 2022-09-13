e31b6572f2c54c77bdda83afa63be6493b97fdc9
seniing <zmmmm111@gmail.com>
2022-09-13 17:43:07 +0900

    [FE] feat: 메인 페이지 구현

--- front/artrend/src/views/accounts/SignUpView.vue
+++ front/artrend/src/views/accounts/SignUpView.vue
@@ -1,37 +1,41 @@
 <template>
-  <div>
-    <img :src="require(`@/assets/login-bg/${state.randomNum}.png`)" alt="img" id="background-img">
-    <nick-name-form></nick-name-form>
-  </div>
+	<div>
+		<img
+			:src="require(`@/assets/login-bg/${state.randomNum}.png`)"
+			alt="img"
+			id="background-img"
+		/>
+		<nick-name-form />
+	</div>
 </template>
 
 <script>
-import { reactive, onBeforeMount } from 'vue';
-import NickNameForm from '@/views/accounts/components/NickNameForm.vue';
+import { reactive, onBeforeMount } from 'vue'
+import NickNameForm from '@/views/accounts/components/NickNameForm.vue'
 
 export default {
-  name: 'SignUpView',
-  components: { NickNameForm },
-  setup() {
-    const state = reactive({
-      randomNum: null,
-    });
-    onBeforeMount(() => {
-      state.randomNum = Math.floor(Math.random() * (9 - 1) + 1);
-    });
-    return {
-      state,
-    };
-  },
-};
+	name: 'SignUpView',
+	components: { NickNameForm },
+	setup() {
+		const state = reactive({
+			randomNum: null,
+		})
+		onBeforeMount(() => {
+			state.randomNum = Math.floor(Math.random() * (9 - 1) + 1)
+		})
+		return {
+			state,
+		}
+	},
+}
 </script>
 
 <style scoped>
 #background-img {
-  height: 100%;
-  width: 100%;
-  filter: brightness(20%);
-  position: absolute;
-  z-index: -1;
+	height: 100%;
+	width: 100%;
+	filter: brightness(20%);
+	position: absolute;
+	z-index: -1;
 }
 </style>

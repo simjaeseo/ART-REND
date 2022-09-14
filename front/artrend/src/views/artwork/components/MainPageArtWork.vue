1460946d225e5c530aacb3ffb2eadb1a3ba35ff0
세룽룽 <zmmmm111@gmail.com>
2022-09-14 17:57:13 +0900

    [FE] feat : 메인페이지 구현

--- front/artrend/src/views/artwork/components/MainPageArtWork.vue
+++ front/artrend/src/views/artwork/components/MainPageArtWork.vue
@@ -1,36 +1,21 @@
 <template>
-	<div>
-		<div class="image-list">
-			<div v-for="image in state.imageNum" :key="image" class="main-image-box">
-				<img
-					:src="require(`@/assets/main-img/${image}.jpg`)"
-					alt="image"
-					class="main-image"
-				/>
-			</div>
-		</div>
-		<!-- <div id="scrollbar">
-			<div class="wrapper">
-				<div
-					v-for="image in state.imageNum"
-					:key="image"
-					class="main-image-box item"
-				>
+	<div class="external">
+		<div class="horizontal-scroll-wrapper">
+			<div v-for="image in state.imageNum" :key="image" class="img-wrapper">
+				<a href="#" target="_blank" rel="noopener">
 					<img
 						:src="require(`@/assets/main-img/${image}.jpg`)"
 						alt="image"
 						class="main-image"
 					/>
-				</div>
+				</a>
 			</div>
-		</div> -->
+		</div>
 	</div>
 </template>
 
 <script>
 import { reactive } from 'vue'
-// import Scrollbar from 'smooth-scrollbar'
-// import gsap from 'gsap'
 
 export default {
 	name: 'MainPageArtWork',
@@ -38,6 +23,7 @@ export default {
 		const state = reactive({
 			imageNum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 		})
+
 		return {
 			state,
 		}
@@ -45,20 +31,71 @@ export default {
 }
 </script>
 
-<style scoped>
-.image-list {
+<style lang="scss" scoped>
+/* hide scrollbar */
+::-webkit-scrollbar {
+	width: 1px;
+	height: 1px;
+}
+
+::-webkit-scrollbar-button {
+	width: 1px;
+	height: 1px;
+}
+
+.external {
+	height: 100vh;
+}
+
+.horizontal-scroll-wrapper {
 	display: flex;
-	flex-wrap: nowrap;
-	align-items: flex-start;
+	flex-direction: column;
+	align-items: center;
+	width: 100vh;
+	transform: rotate(-90deg) translate3d(0, -100vh, 0);
+	transform-origin: right top;
+	overflow-y: auto;
+	overflow-x: hidden;
+	padding: 0;
+	height: 100vw;
+	transform-style: preserve-3d;
+	perspective: 1px;
+	padding-bottom: 10rem;
+}
+.img-wrapper {
+	transform: rotate(90deg);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	// min-height: 70vh;
+	min-height: 40vh;
+	transform-origin: 50% 50%;
+	transform: rotate(90deg) translateZ(0.1px) translateX(0px) translateY(-3vh);
+	transition: 1s;
 }
 
-.main-image-box {
-	height: 70vh;
-	padding: 0% 1.5%;
+.img-wrapper:hover {
+	// min-height: 90vh;
+	min-height: 65vh;
+	// transform: rotate(90deg) scale(1.1);
 }
 
-.main-image {
+.img-wrapper a {
+	overflow: hidden;
 	display: block;
-	height: 100%;
+	box-shadow: 0px 0px 80px rgb(255, 255, 255, 0.25);
+	padding: 1vh;
+}
+
+img {
+	max-height: 60vh;
+	transition: 0.5s;
+	vertical-align: top;
+	max-width: 45vh;
+	filter: saturate(40%) sepia(30%) hue-rotate(5deg);
+}
+
+a:hover img {
+	filter: none;
 }
 </style>

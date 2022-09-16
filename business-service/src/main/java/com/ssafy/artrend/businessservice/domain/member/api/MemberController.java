7380aff35cdab69b82a50bc216bf9027c9ffef55
simjaeseo <delpho@naver.com>
2022-09-16 20:36:07 +0900

    [BE] feat: 프로젝트 초기 세팅 (apigateway, auth, business, discovery)

---
+++ business-service/src/main/java/com/ssafy/artrend/businessservice/domain/member/api/MemberController.java
@@ -0,0 +1,30 @@
+package com.ssafy.artrend.businessservice.domain.member.api;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/business-service")
+@AllArgsConstructor
+@Slf4j
+public class MemberController {
+
+    final Environment env;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("check")
+    public String check(){
+        log.info("Server port={}", env.getProperty("local.server.port"));
+
+        return String.format("This Service port is %s", env.getProperty("local.server.port"));
+    }
+
+}

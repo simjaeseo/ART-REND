945ff01759c2aaba4a3f831df60cc644715a23c7
simjaeseo <delpho@naver.com>
2022-09-17 01:12:32 +0900

    [BE] feat: cloud config, cloud bus 관련 코드 추가

---
+++ auth-service/src/main/java/com/ssafy/artrend/authservice/global/common/MessageResponse.java
@@ -0,0 +1,16 @@
+package com.ssafy.artrend.authservice.global.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class MessageResponse<T> {
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    public MessageResponse() {
+        this.httpStatus = HttpStatus.OK;
+        this.message = "success";
+    }
+}

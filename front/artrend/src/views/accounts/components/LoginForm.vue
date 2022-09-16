0aa641063c553b52ca20607f5eb2482bb87d70ae
gitjisu <kimvely0422@gmail.com>
2022-09-16 20:23:26 +0900

    [FE]feat: kakao로그인

--- front/artrend/src/views/accounts/components/LoginForm.vue
+++ front/artrend/src/views/accounts/components/LoginForm.vue
@@ -24,39 +24,68 @@
 <script>
 export default {
 	name: 'LoginForm',
-	setup() {
-		const loginWithKakao = function () {
-			const params = {
-				redirectUri: 'http://localhost:8080/auth',
-			}
-			window.Kakao.Auth.authorize(params)
-		}
+	// setup() {
+	// 	const loginWithKakao = function () {
+	// 		const params = {
+	// 			redirectUri: 'http://localhost:8080/auth',
+	// 		}
+	// 		window.Kakao.Auth.authorize(params)
+	// 	}
 
-		return {
-			loginWithKakao,
-		}
-	},
+	// 	return {
+	// 		loginWithKakao,
+	// 	}
+	// },
 	data() {
 		return {
 			user: '',
 		}
 	},
 	methods: {
-		async loginWithGoogle() {
-			try {
-				const googleUser = await this.$gAuth.signIn()
-
-				if (!googleUser) {
-					return null
-				}
-
-				this.user = googleUser.getBasicProfile().getEmail()
-			} catch (error) {
-				console.log(error)
-				return null
-			}
-			return null
+		loginWithKakao() {
+			console.log(window.Kakao)
+			window.Kakao.Auth.login({
+				scope: 'profile_nickname, account_email, gender, age_range',
+				success: this.getKakaoAccount,
+			})
 		},
+		getKakaoAccount() {
+			window.Kakao.API.request({
+				url: '/v2/user/me',
+				success: res => {
+					const kakao_account = res.kakao_account
+					const nickname = kakao_account.profile.nickname
+					const email = kakao_account.email
+					const gender = kakao_account.gender
+					const age_range = kakao_account.age_range
+					console.log(kakao_account)
+					console.log(nickname)
+					console.log(email)
+					console.log(gender)
+					console.log(age_range)
+					console.log(localStorage.getItem('token'))
+					alert('로그인성공!')
+				},
+				fail: error => {
+					console.log(error)
+				},
+			})
+		},
+		// async loginWithGoogle() {
+		// 	try {
+		// 		const googleUser = await this.$gAuth.signIn()
+
+		// 		if (!googleUser) {
+		// 			return null
+		// 		}
+
+		// 		this.user = googleUser.getBasicProfile().getEmail()
+		// 	} catch (error) {
+		// 		console.log(error)
+		// 		return null
+		// 	}
+		// 	return null
+		// },
 	},
 }
 </script>

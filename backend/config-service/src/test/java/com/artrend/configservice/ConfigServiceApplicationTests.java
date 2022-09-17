be1626cc8410ee993228e6be158c5c665b7acb2d
jg6735 <jg6735@naver.com>
2022-09-18 04:39:48 +0900

    [Be] build: Config Server 폴더 구조 및 프로젝트명 변경

--- backend/config-service/src/test/java/com/artrend/configservice/ConfigServiceApplicationTests.java
+++ backend/config-service/src/test/java/com/artrend/configservice/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ssafy.artrend.configserver;
+package com.artrend.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {

720ce43b0a452d5893599cbcfc7e34cd2c38dded
jg6735 <jg6735@naver.com>
2022-09-18 05:10:06 +0900

    [BE] build: API Gateway Server 의존성 추가, 포트 및 프로젝트명 변경

--- backend/apigateway-service/src/test/java/com/artrend/apigatewayservice/ApigatewayServiceApplicationTests.java
+++ backend/apigateway-service/src/test/java/com/artrend/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ssafy.artrend.apigatewayservice;
+package com.artrend.apigatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

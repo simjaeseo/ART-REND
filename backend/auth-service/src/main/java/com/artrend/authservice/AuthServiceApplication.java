310f804db09950301600a9863e67eb40a5566c02
jg6735 <jg6735@naver.com>
2022-09-18 05:45:07 +0900

    [Be] build: Auth Service 의존성 추가 및 패키지 구조 변경

--- backend/auth-service/src/main/java/com/artrend/authservice/AuthServiceApplication.java
+++ backend/auth-service/src/main/java/com/artrend/authservice/AuthServiceApplication.java
@@ -1,8 +1,10 @@
-package com.ssafy.artrend.authservice;
+package com.artrend.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AuthServiceApplication {
 
--- backend/auth-service/src/main/java/com/artrend/authservice/global/common/MessageResponse.java
+++ backend/auth-service/src/main/java/com/artrend/authservice/global/common/MessageResponse.java
@@ -1,4 +1,4 @@
-package com.ssafy.artrend.authservice.global.common;
+package com.artrend.authservice.global.common;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;

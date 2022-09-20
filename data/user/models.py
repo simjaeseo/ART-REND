a7a0dc81d2b01cb91e086ac2bdc2546ca94410fd
sjhyun1006 <sjhyun1006@naver.com>
2022-09-20 17:00:38 +0900

    [DT] feat : mysql model connection

--- data/user/models.py
+++ data/user/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
 class Member(models.Model):
     member_id = models.BigAutoField(primary_key=True)
-    created_date = models.DateTimeField(blank=True, null=True)
-    favorite_style_id = models.BigIntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     nickname = models.CharField(max_length=255, blank=True, null=True)
-    updated_date = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = False

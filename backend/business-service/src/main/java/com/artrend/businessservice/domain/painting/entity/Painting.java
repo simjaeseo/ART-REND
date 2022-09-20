e7ff11533b3388873b82caabd72d6397a4dc58ab
jg6735 <jg6735@naver.com>
2022-09-20 15:07:20 +0900

    [BE] fix: 그림 기본키 생성전략 추가

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/entity/Painting.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/entity/Painting.java
@@ -5,10 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,7 +13,7 @@ import javax.persistence.Id;
 public class Painting extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "painting_id")
     private Long id;
 

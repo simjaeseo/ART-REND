857e9de4e4c244cfb6e2b8bc00225a1370222a06
jg6735 <jg6735@naver.com>
2022-09-22 04:47:29 +0900

    [BE] feat: 그림 기반 엔티티들 기본적인 셋팅

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/ChangedPaintingRepository.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/ChangedPaintingRepository.java
@@ -3,5 +3,5 @@ package com.artrend.businessservice.domain.painting.repository;
 import com.artrend.businessservice.domain.painting.entity.ChangedPainting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChangedPaintingRepository extends JpaRepository<ChangedPainting,Long> {
+public interface ChangedPaintingRepository extends JpaRepository<ChangedPainting, Long> {
 }

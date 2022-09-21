09f413b945d68dfedf6ca1159f47e27db8d6d03e
jg6735 <jg6735@naver.com>
2022-09-22 05:25:51 +0900

    [BE] feat: SwaggerDoc API 명세서 추가

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/api/LikedPaintingController.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/api/LikedPaintingController.java
@@ -3,6 +3,9 @@ package com.artrend.businessservice.domain.painting.api;
 import com.artrend.businessservice.domain.painting.dto.LikedPaintingDto;
 import com.artrend.businessservice.domain.painting.service.LikedPaintingService;
 import com.artrend.businessservice.global.common.DataResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,13 @@ import java.io.IOException;
 public class LikedPaintingController {
     private final LikedPaintingService likedPaintingService;
 
+    @Operation(summary = "그림 좋아요", description = "현재 접속한 회원 정보로 해당 그림을 좋아요합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 접근입니다."),
+            @ApiResponse(responseCode = "404", description = "그림이 존재하지 않습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+    })
     @PostMapping
     public ResponseEntity<? extends DataResponse> like(@RequestBody @Valid
                                                            LikedPaintingDto likedPaintingDto) throws IOException {
@@ -26,6 +36,13 @@ public class LikedPaintingController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new DataResponse(likedPaintingDto));
     }
 
+    @Operation(summary = "그림 좋아요 취소", description = "현재 접속한 회원 정보로 해당 그림을 좋아요 취소합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 접근입니다."),
+            @ApiResponse(responseCode = "404", description = "그림이 존재하지 않습니다."),
+            @ApiResponse(responseCode = "500", description = "서버 에러입니다.")
+    })
     @DeleteMapping
     public ResponseEntity<? extends DataResponse> cancelLike(@RequestBody @Valid
                                                              LikedPaintingDto likedPaintingDto) throws IOException {

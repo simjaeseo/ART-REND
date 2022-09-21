b0bfd3742cedb7ce18912416b5edd07da27a8df7
Jisu Kim <kimvely0422@gmail.com>
2022-09-21 17:53:08 +0900

    [FE] feat: nick name data vinding

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,13 +8,15 @@ import DetailPageView from '@/views/artwork/DetailPageView.vue'
 import ArtWorksArrayView from '@/views/artwork/ArtWorksArrayView.vue'
 import KakaoAuthView from '@/views/accounts/KakaoAuthView.vue'
 
-const requireAuth = () => (from, to, next) => {
-	const token = localStorage.getItem('token')
-	if (token) {
-		return next()
-	}
-	next('/')
-}
+// 라우터가드는 개발 끝나고 주석 풀면 됨
+// const requireAuth = () => (from, to, next) => {
+// 	const token = localStorage.getItem('token')
+// 	if (token) {
+// 		return next()
+// 	}
+// 	alert('로그인이 필요한 서비스입니다.')
+// 	next('/')
+// }
 
 const routes = [
 	{
@@ -26,37 +28,37 @@ const routes = [
 		path: '/signup',
 		name: 'SignUp',
 		component: SignUpView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/select',
 		name: 'SelectImage',
 		component: SelectImageView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/mypage',
 		name: 'MyPage',
 		component: MyPageView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/main',
 		name: 'Main',
 		component: MainPageView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/detail',
 		name: 'Detail',
 		component: DetailPageView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/artworks',
 		name: 'Artworks',
 		component: ArtWorksArrayView,
-		beforeEnter: requireAuth(),
+		// beforeEnter: requireAuth(),
 	},
 	{
 		path: '/auth',

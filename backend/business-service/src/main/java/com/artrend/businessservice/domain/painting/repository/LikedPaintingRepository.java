a3db5a005ac0a3d6041a582c31397a87a11099c5
jg6735 <jg6735@naver.com>
2022-09-22 16:04:58 +0900

    [BE] fix: 회원 엔티티 삭제, 장르 기반 추천 테이블 생성

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/LikedPaintingRepository.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/LikedPaintingRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LikedPaintingRepository extends JpaRepository<LikedPainting, Long> {
-    Optional<LikedPainting> findLikedPaintingByMemberAndPaintingId(Member member, Long paintingId);
+//    Optional<LikedPainting> findLikedPaintingByMemberAndPaintingId(Member member, Long paintingId);
 }
--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/service/LikedPaintingServiceImpl.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/service/LikedPaintingServiceImpl.java
@@ -3,7 +3,6 @@ package com.artrend.businessservice.domain.painting.service;
 import com.artrend.businessservice.domain.member.entity.Member;
 import com.artrend.businessservice.domain.member.exception.MemberException;
 import com.artrend.businessservice.domain.member.exception.MemberExceptionType;
-import com.artrend.businessservice.domain.member.repository.MemberRepository;
 import com.artrend.businessservice.domain.painting.dto.LikedPaintingDto;
 import com.artrend.businessservice.domain.painting.entity.LikedPainting;
 import com.artrend.businessservice.domain.painting.entity.Painting;
@@ -11,8 +10,11 @@ import com.artrend.businessservice.domain.painting.exception.PaintingException;
 import com.artrend.businessservice.domain.painting.exception.PaintingExceptionType;
 import com.artrend.businessservice.domain.painting.repository.LikedPaintingRepository;
 import com.artrend.businessservice.domain.painting.repository.PaintingRepository;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +28,8 @@ import java.util.Optional;
 public class LikedPaintingServiceImpl implements LikedPaintingService {
     private final PaintingRepository paintingRepository;
     private final LikedPaintingRepository likedPaintingRepository;
-    private final MemberRepository memberRepository;
+    @Value("${jwt.secret}")
+    private String secret;
 
     @Override
     @Transactional
@@ -41,7 +44,7 @@ public class LikedPaintingServiceImpl implements LikedPaintingService {
         // 3. 좋아요 한 그림 ID와 회원 ID로 DB에 저장하기
         LikedPainting likedPainting = LikedPainting.builder()
                 .paintingId(likedPaintingDto.getPaintingId())
-                .member(memberRepository.findMemberById(likedPaintingDto.getMemberId()))
+//                .member(memberRepository.findMemberById(likedPaintingDto.getMemberId()))
                 .build();
 
         likedPaintingRepository.save(likedPainting);
@@ -70,15 +73,22 @@ public class LikedPaintingServiceImpl implements LikedPaintingService {
 
     public void validateToken(LikedPaintingDto likedPaintingDto) {
         // 유효한 토큰인지 검증 (auth-service 요청 필요)
+        String token = HttpHeaders.AUTHORIZATION;
+        Long id = Long.parseLong(
+                        Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody().getId()
+                );
+
+        if (id == null) {
+            throw new MemberException(MemberExceptionType.NOT_FOUND_MEMBER);
+        }
     }
 
     public Optional<LikedPainting> findLikedPaintingWithMemberAndPaintingId(LikedPaintingDto likedPaintingDto) {
-        // 존재하는 회원이 아닌 경우 에러 호출하기
-        Member findMember = memberRepository.findById(likedPaintingDto.getMemberId())
-                .orElseThrow(() -> new MemberException(MemberExceptionType.NOT_FOUND_MEMBER));
 
-        return likedPaintingRepository
-                .findLikedPaintingByMemberAndPaintingId(findMember, likedPaintingDto.getPaintingId());
+//        return likedPaintingRepository
+//                .findLikedPaintingByMemberAndPaintingId(findMember, likedPaintingDto.getPaintingId());
+
+        return null;
     }
 
     public void updateLikeCount(Long paintingId, Integer count) throws IOException {

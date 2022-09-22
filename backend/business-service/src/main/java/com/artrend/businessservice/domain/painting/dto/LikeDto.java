6c8cff22f932e0edbbfe7ca231bf9d6264753f13
jg6735 <jg6735@naver.com>
2022-09-22 23:09:49 +0900

    [BE] feat: 좋아요한 그림 조회 기능 구현

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/dto/LikeDto.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/dto/LikeDto.java
@@ -1,5 +1,6 @@
 package com.artrend.businessservice.domain.painting.dto;
 
+import com.artrend.businessservice.domain.painting.entity.Painting;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class LikedPaintingDto {
+public class LikeDto {
     private Long paintingId;
     private Long memberId;
 }

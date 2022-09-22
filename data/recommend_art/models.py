c8fce12528aa8661976f9874c48ee9b8e4b7a5d7
sjhyun1006 <sjhyun1006@naver.com>
2022-09-23 00:41:12 +0900

    [DT] fix : error

--- data/recommend_art/models.py
+++ data/recommend_art/models.py
@@ -1,33 +1,53 @@
 from django.db import models
-from user.models import AuthUser
+# from user.models import AuthUser
 # Create your models here.
+from django.conf import settings
+
+class Painting(models.Model):
+    painting_id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    art_trend = models.CharField(max_length=255, blank=True, null=True)
+    artist = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=5000, blank=True, null=True)
+    dimensions = models.CharField(max_length=255, blank=True, null=True)
+    genre = models.CharField(max_length=255, blank=True, null=True)
+    hits = models.BigIntegerField(blank=True, null=True)
+    korean_title = models.CharField(max_length=255, blank=True, null=True)
+    medium = models.CharField(max_length=255, blank=True, null=True)
+    place = models.CharField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    total_change_count = models.BigIntegerField(blank=True, null=True)
+    total_like_count = models.BigIntegerField(blank=True, null=True)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    year = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'painting'
+        
+        
 class ChangedPainting(models.Model):
     changed_painting_id = models.BigAutoField(primary_key=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    member_id = models.BigIntegerField(blank=True, null=True)
     url = models.CharField(max_length=255, blank=True, null=True)
-    painting_painting = models.ForeignKey('Painting', models.DO_NOTHING, blank=True, null=True)
-    member = models.ForeignKey('Member', models.DO_NOTHING, blank=True, null=True)
-    painting = models.ForeignKey('Painting', models.DO_NOTHING, blank=True, null=True)
+    painting = models.ForeignKey(Painting, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'changed_painting'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
+        db_table = 'changed_painting'   
+        
+            
+class DetailRecommendedPainting(models.Model):
+    detail_recommended_painting_id = models.BigAutoField(primary_key=True)
+    painting = models.ForeignKey(Painting, models.DO_NOTHING, blank=True, null=True, related_name="painting")
+    recommended_painting_painting = models.ForeignKey(Painting, models.DO_NOTHING, blank=True, null=True, related_name="recommended_painting")
+    
     class Meta:
         managed = False
-        db_table = 'django_admin_log'
-
+        db_table = 'detail_recommended_painting'
 
 class DjangoContentType(models.Model):
     app_label = models.CharField(max_length=100)
@@ -37,6 +57,22 @@ class DjangoContentType(models.Model):
         managed = False
         db_table = 'django_content_type'
         unique_together = (('app_label', 'model'),)
+        
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey(DjangoContentType, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+
 
 
 class DjangoMigrations(models.Model):
@@ -61,8 +97,7 @@ class DjangoSession(models.Model):
 
 
 class FavoriteStyle(models.Model):
-    favorite_style_id = models.BigAutoField(primary_key=True)
-    painting = models.ForeignKey('Painting', models.DO_NOTHING)
+    painting = models.OneToOneField(Painting, models.DO_NOTHING, primary_key=True)
 
     class Meta:
         managed = False
@@ -71,8 +106,8 @@ class FavoriteStyle(models.Model):
 
 class LikedPainting(models.Model):
     liked_painting_id = models.BigAutoField(primary_key=True)
+    member_id = models.BigIntegerField(blank=True, null=True)
     painting_id = models.BigIntegerField(blank=True, null=True)
-    member = models.ForeignKey('Member', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -89,32 +124,12 @@ class Member(models.Model):
         db_table = 'member'
 
 
-class Painting(models.Model):
-    painting_id = models.BigAutoField(primary_key=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    art_trend = models.CharField(max_length=255, blank=True, null=True)
-    artist = models.CharField(max_length=255, blank=True, null=True)
-    description = models.CharField(max_length=5000, blank=True, null=True)
-    dimensions = models.CharField(max_length=255, blank=True, null=True)
-    genre = models.CharField(max_length=255, blank=True, null=True)
-    hits = models.BigIntegerField(blank=True, null=True)
-    korean_title = models.CharField(max_length=255, blank=True, null=True)
-    medium = models.CharField(max_length=255, blank=True, null=True)
-    place = models.CharField(max_length=255, blank=True, null=True)
-    title = models.CharField(max_length=255, blank=True, null=True)
-    total_change_count = models.BigIntegerField(blank=True, null=True)
-    total_like_count = models.BigIntegerField(blank=True, null=True)
-    url = models.CharField(max_length=255, blank=True, null=True)
-    year = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'painting'
 
 
 class RecommendedPainting(models.Model):
     liked_painting_id = models.BigAutoField(primary_key=True)
+    member_id = models.BigIntegerField(blank=True, null=True)
+    painting = models.ForeignKey(Painting, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -123,8 +138,8 @@ class RecommendedPainting(models.Model):
 
 class SelectedPainting(models.Model):
     selected_painting_id = models.BigAutoField(primary_key=True)
+    member_id = models.BigIntegerField(blank=True, null=True)
     painting = models.ForeignKey(FavoriteStyle, models.DO_NOTHING, blank=True, null=True)
-    member = models.ForeignKey(Member, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False

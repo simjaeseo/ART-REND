35ba2fd18e1ee3a6feb0afca07a65b7e5d51e107
seniing <zmmmm111@gmail.com>
2022-09-23 22:07:20 +0900

    [FE] feat: 닉네임 입력 및 회원가입 사진 선택 구현

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,14 @@
-const HOST = 'http://localhost:8080/'
-
 const AUTH = 'auth-service/'
-// const BUSSiNESS = 'bussiness-service/'
+const BUSiNESS = 'business-service/'
 // const RECOMMEND = 'recommend-service/'
 
 export default {
 	auth: {
-		kakaoLogin: () => `${HOST + AUTH}oauth2/authorization/kakao`,
-		nickNameForm: memberId => `${HOST + AUTH}signup/${memberId}/nickname`,
-		nickNameCheck: memberId =>
-			`${HOST + AUTH}signup/${memberId}/nickname/duplicate`,
+		kakaoLogin: () => `${AUTH}oauth2/authorization/kakao`,
+		nickNameForm: memberId => `${AUTH}signup/${memberId}/nickname`,
+		nickNameCheck: memberId => `${AUTH}signup/${memberId}/nickname/duplicate`,
+	},
+	business: {
+		select: () => `${BUSiNESS}api/select`,
 	},
 }

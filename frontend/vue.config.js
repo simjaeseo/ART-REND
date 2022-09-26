5c9ac664beb550dfc28b3b10ca35509e35f80ec6
세룽룽 <zmmmm111@gmail.com>
2022-09-26 17:59:17 +0900

    [FE] fix: api 변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,32 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
 	transpileDependencies: true,
-	devServer: {
-		proxy: {
-			'/auth-service': {
-				target: 'http://localhost:8080/',
-				changeOrigin: true,
-				logLevel: 'debug',
-			},
-			'/business-service': {
-				target: 'http://localhost:8080/',
-				pathRewrite: { '^/detail': '/', '^/mypage': '/', '^/artist': '/' },
-				changeOrigin: true,
-				logLevel: 'debug',
-			},
-		},
-	},
+	// devServer: {
+	// 	proxy: {
+	// 		'/auth-service': {
+	// 			target: 'http://localhost:8080/',
+	// 			changeOrigin: true,
+	// 			logLevel: 'debug',
+	// 		},
+	// 		'/business-service': {
+	// 			target: 'http://localhost:8080/',
+	// 			pathRewrite: { '^/detail': '/', '^/mypage': '/', '^/artist': '/' },
+	// 			changeOrigin: true,
+	// 			logLevel: 'debug',
+	// 		},
+	// 	},
+	// },
 })
-
-// module.exports = {
-// 	css: {
-// 		loaderOptions: {
-// 			sass: {
-// 				data: `
-// 						@import "@/style/style.scss";
-// 						//@는 /src 와 같다
-// 				`,
-// 			},
-// 		},
-// 	},
-// }

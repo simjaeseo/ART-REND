bdad47790dbe4ac92f07f9b5fa634e2042612ca5
심재서 <delpho@naver.com>
2022-09-26 17:40:19 +0900

    Update backend/apigateway-service/src/main/resources/application.yml, backend/business-service/src/main/resources/application.yml, backend/auth-service/src/main/resources/application.yml, backend/auth-service/src/main/java/com/artrend/authservice/global/config/WebConfig.java
    Former-commit-id: d696a4e3dfd12e1ef22849705b26ba1a487704f3

--- backend/auth-service/src/main/java/com/artrend/authservice/global/config/WebConfig.java
+++ backend/auth-service/src/main/java/com/artrend/authservice/global/config/WebConfig.java
@@ -8,16 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name()
-                );
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //             .allowedOrigins("*")
+    //             .allowedMethods(
+    //                     HttpMethod.GET.name(),
+    //                     HttpMethod.HEAD.name(),
+    //                     HttpMethod.POST.name(),
+    //                     HttpMethod.PUT.name(),
+    //                     HttpMethod.DELETE.name()
+    //             );
+    // }
 }

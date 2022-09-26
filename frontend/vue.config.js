a03329472afd5a9fd2059fa9e79bd99f58cd1a48
jg6735 <jg6735@naver.com>
2022-09-27 02:35:57 +0900

    [BE] feat: 모든 서버가 Config Server 기반으로 설정 정보가 구성되도록 변경 완료
    Former-commit-id: 2f13be9a3affbbc94e050dd691c15586d5893c81

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,21 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
 	transpileDependencies: true,
-	devServer: {
-		proxy: {
-			'/auth-service': {
-				target: 'http://localhost:8080/',
-				changeOrigin: true,
-				logLevel: 'debug',
-			},
-			'/business-service': {
-				target: 'http://localhost:8080/',
-				pathRewrite: { '^/detail': '/', '^/mypage': '/', '^/artist': '/' },
-				changeOrigin: true,
-				logLevel: 'debug',
-			},
-		},
-	},
+	// devServer: {
+	// 	proxy: {
+	// 		'/auth-service': {
+	// 			target: 'http://localhost:8080/',
+	// 			changeOrigin: true,
+	// 			logLevel: 'debug',
+	// 		},
+	// 		'/business-service': {
+	// 			target: 'http://localhost:8080/',
+	// 			pathRewrite: { '^/detail': '/', '^/mypage': '/', '^/artist': '/' },
+	// 			changeOrigin: true,
+	// 			logLevel: 'debug',
+	// 		},
+	// 	},
+	// },
 })
 
 // module.exports = {

bf865243a6f02a6938008830af4c48fa6c1cc7f4
gitjisu <kimvely0422@gmail.com>
2022-09-27 21:23:45 +0900

    [FE]
    Former-commit-id: 47e3fee5defa9073daefe1f31a23c3dacbbb05e5

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/ChangedPaintingRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.artrend.businessservice.domain.painting.repository;
-
-import com.artrend.businessservice.domain.painting.entity.ChangedPainting;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ChangedPaintingRepositoryCustom {
-    Page<ChangedPainting> findChangedPaintingsAll(Long memberId, Pageable pageable);
-}
--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/RecommendedPaintingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.artrend.businessservice.domain.painting.repository;
-
-import com.artrend.businessservice.domain.painting.entity.RecommendedPainting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendedPaintingRepository extends JpaRepository<RecommendedPainting, Long>, RecommendedPaintingRepositoryCustom {
-
-}

da7d2c24954720060e276d2a4cb76df09f38f615
jg6735 <jg6735@naver.com>
2022-09-28 14:04:48 +0900

    [BE] feat: 추천 데이터 넘겨주기 구현
    Former-commit-id: 843963a11340cbb89b7a5c9761f6f8487e0b664d

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/service/LikedPaintingService.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/service/LikedPaintingService.java
@@ -4,14 +4,13 @@ import com.artrend.businessservice.domain.painting.dto.MemberDto;
 import com.artrend.businessservice.domain.painting.dto.LikedPaintingDto;
 import com.artrend.businessservice.domain.painting.dto.PaintingDto;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface LikedPaintingService {
-    void like(MemberDto memberDto) throws IOException;
+    ResponseEntity<Object> like(MemberDto memberDto) throws IOException;
     void cancelLike(MemberDto memberDto) throws IOException;
     List<LikedPaintingDto> findLikedPaintings(Long memberId, Pageable pageable);
-    void recommendRequestV1(Long paintingId);
-    PaintingDto recommendRequestV2(Long paintingId);
 }

58cd0d788a69003d868e3c34b03f5acd6e825757
jg6735 <jg6735@naver.com>
2022-09-30 03:49:35 +0900

    [BE] fix: 패키지 구조 변경
    Former-commit-id: 6a2aae58e2a36baaf73451fa398359161602eed8

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/RecommendedPaintingRepository.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/RecommendedPaintingRepository.java
@@ -1,6 +1,7 @@
 package com.artrend.businessservice.domain.painting.repository;
 
 import com.artrend.businessservice.domain.painting.entity.RecommendedPainting;
+import com.artrend.businessservice.domain.painting.repository.support.RecommendedPaintingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecommendedPaintingRepository extends JpaRepository<RecommendedPainting, Long>, RecommendedPaintingRepositoryCustom {
--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/support/DetailRecommendedPaintingRepositoryCustom.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/support/DetailRecommendedPaintingRepositoryCustom.java
@@ -1,4 +1,4 @@
-package com.artrend.businessservice.domain.painting.repository;
+package com.artrend.businessservice.domain.painting.repository.support;
 
 import com.artrend.businessservice.domain.painting.entity.DetailRecommendedPainting;
 import org.springframework.data.domain.Page;

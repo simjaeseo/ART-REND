810932ae4a090f11c633318743b3cdf0355ee565
jg6735 <jg6735@naver.com>
2022-09-30 03:49:35 +0900

    [BE] fix: 패키지 구조 변경
    Former-commit-id: 732559abc0e40605702f358f8d36a83ad3a32451

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/LikedPaintingRepository.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/painting/repository/LikedPaintingRepository.java
@@ -1,6 +1,7 @@
 package com.artrend.businessservice.domain.painting.repository;
 
 import com.artrend.businessservice.domain.painting.entity.LikedPainting;
+import com.artrend.businessservice.domain.painting.repository.support.LikedPaintingRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;

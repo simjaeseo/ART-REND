45447d4d2b12f1fd5ae20312191e74b587cf9596
jg6735 <jg6735@naver.com>
2022-09-30 02:58:36 +0900

    [BE] fix: 분류별 그림 조회 에러 해결
    Former-commit-id: 07bacbffddc942bc6e160e622a6c60108bf3a91c

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/description/repository/DescriptionQueryRepository.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/description/repository/DescriptionQueryRepository.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
-import java.util.List;
-
 import static com.artrend.businessservice.domain.description.entity.QArtTrend.artTrend;
 import static com.artrend.businessservice.domain.description.entity.QArtist.artist;
 import static com.artrend.businessservice.domain.description.entity.QGenre.genre;
@@ -27,81 +25,34 @@ public class DescriptionQueryRepository {
                 .select(new QArtistDto(artist))
                 .from(artist)
                 .where(
-                        artistContains(condition.getName())
+                        artistEq(condition.getName())
                 ).fetchOne();
     }
 
     public GenreDto searchGenreV1(DetailCondition condition) {
-        List<GenreDto> fetch = queryFactory
+        return queryFactory
                 .select(new QGenreDto(genre))
                 .from(genre)
                 .where(
-                        genreContains(condition.getName())
-                ).fetch();
-
-        GenreDto genreDto = null;
-
-        if (fetch.size() > 1) {
-            genreDto = queryFactory
-                    .select(new QGenreDto(genre))
-                    .from(genre)
-                    .where(
-                            genreEq(condition.getName())
-                    ).fetchOne();
-        }
-
-        return genreDto;
+                        genreEq(condition.getName())
+                ).fetchOne();
     }
-
 
     public ArtTrendDto searchArtTrendV1(DetailCondition condition) {
-        List<ArtTrendDto> fetch = queryFactory
-                .select(new QArtTrendDto(artTrend))
-                .from(artTrend)
-                .where(
-                        artTrendContains(condition.getName())
-                ).fetch();
-
-        ArtTrendDto artTrendDto = null;
-
-        if (fetch.size() > 1) {
-            artTrendDto = queryFactory
-                    .select(new QArtTrendDto(artTrend))
-                    .from(artTrend)
-                    .where(
-                            artTrendEq(condition.getName())
-                    ).fetchOne();
-        }
-
-        return artTrendDto;
-    }
-
-    public ArtTrendDto searchArtTrendV2(DetailCondition condition) {
         return queryFactory
                 .select(new QArtTrendDto(artTrend))
                 .from(artTrend)
                 .where(
+                        artTrendEq(condition.getName())
                 ).fetchOne();
-    }
-
-    private BooleanExpression artistContains(String name) {
-        return hasText(name) ? artist.name.contains(name) : null;
-    }
-
-    private BooleanExpression genreContains(String name) {
-        return hasText(name) ? genre.name.contains(name) : null;
     }
 
     private BooleanExpression genreEq(String name) {
         return hasText(name) ? genre.name.eq(name) : null;
     }
 
-    private BooleanExpression artTrendContains(String name) {
-        return hasText(name) ? artTrend.name.contains(name) : null;
-    }
-
-    private BooleanExpression artTrendContainsLt(String name) {
-        return hasText(name) ? artTrend.name.contains(name).count().lt(2) : null;
+    private BooleanExpression artistEq(String name) {
+        return hasText(name) ? artist.name.eq(name) : null;
     }
 
     private BooleanExpression artTrendEq(String name) {

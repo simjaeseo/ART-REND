c6332004604822ce07a5df825f54ba8875e3e953
jg6735 <jg6735@naver.com>
2022-10-01 23:37:22 +0900

    [BE] fix: 메인 페이지 추천그림 갱신할 액션로그 변환값 추가
    Former-commit-id: a627cfb4592960a9d197b2f8c3cf852fc97aa9de

--- backend/business-service/src/main/java/com/artrend/businessservice/domain/log/vo/MemberLogRequest.java
+++ backend/business-service/src/main/java/com/artrend/businessservice/domain/log/vo/MemberLogRequest.java
@@ -7,6 +7,7 @@ public class MemberLogRequest {
     private Long memberId;
     private Long paintingId;
     private Long clickCount;
+    private Long changeCount;
     private Long zoomCount;
     private String inTime;
     private String outTime;

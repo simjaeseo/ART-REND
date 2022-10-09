da22dd849ada10583a256129953f199bc06d2a6c
jg6735 <jg6735@naver.com>
2022-10-10 02:33:56 +0900

    [FE] fix: exhibition page CSS fix
    Former-commit-id: 4e1d7610eecb78a07a67a4d73bcb9bd7713e265e

--- frontend/src/views/artwork/components/ExhibitionAll.vue
+++ frontend/src/views/artwork/components/ExhibitionAll.vue
@@ -39,6 +39,29 @@ export default {
 		const goDetail = function (userId) {
 			window.location.href = `http://j7c104.p.ssafy.io/mypage/${userId}`
 		}
+		window.onload = function () {
+			function masonryLayout() {
+				const masonryContainerStyle = getComputedStyle(
+					document.querySelector('.masonry-container'),
+				)
+				const columnGap = parseInt(
+					masonryContainerStyle.getPropertyValue('column-gap'),
+				)
+				const autoRows = parseInt(
+					masonryContainerStyle.getPropertyValue('grid-auto-rows'),
+				)
+
+				document.querySelectorAll('.masonry-item').forEach(elt => {
+					elt.style.gridRowEnd = `span ${Math.ceil(
+						elt.querySelector('.pseudo-img').scrollHeight / autoRows +
+							columnGap / autoRows,
+					)}`
+				})
+			}
+
+			masonryLayout()
+			window.addEventListener('resize', masonryLayout)
+		}
 		return {
 			convertList,
 			goDetail,

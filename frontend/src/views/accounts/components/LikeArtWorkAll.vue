c932131f95be783820e2613a9f6c9f65d5c5a9aa
seniing <zmmmm111@gmail.com>
2022-10-12 01:07:48 +0900

    [FE] fix: mypage 오류 수정
    Former-commit-id: d1a5a80dffca85ca78ab46f1a25e93cad42e1120

--- frontend/src/views/accounts/components/LikeArtWorkAll.vue
+++ frontend/src/views/accounts/components/LikeArtWorkAll.vue
@@ -70,30 +70,6 @@ export default {
 			window.location.href = `http://localhost:3002/detail/${artworkId}`
 		}
 
-		function masonryLayout() {
-			const masonryContainerStyle = getComputedStyle(
-				document.querySelector('.masonry-container'),
-			)
-			const columnGap = parseInt(
-				masonryContainerStyle.getPropertyValue('column-gap'),
-			)
-			const autoRows = parseInt(
-				masonryContainerStyle.getPropertyValue('grid-auto-rows'),
-			)
-
-			document.querySelectorAll('.masonry-item').forEach(elt => {
-				elt.style.gridRowEnd = `span ${Math.ceil(
-					elt.querySelector('.pseudo-img').scrollHeight / autoRows +
-						columnGap / autoRows,
-				)}`
-			})
-		}
-
-		window.onload = function () {
-			masonryLayout()
-			window.addEventListener('resize', masonryLayout)
-		}
-
 		return {
 			state,
 			unlikeArtWork,
@@ -175,6 +151,10 @@ export default {
 	transition: 0.5s;
 	vertical-align: top;
 	filter: grayscale(0%);
+}
+
+a {
+	width: 100%;
 }
 
 img {
